4dfd4975bcb0b1f7adff7a4d766ba8549fa66e5a
OleksandrB93 <barabashsan4ik@gmail.com>
2023-02-21 22:52:56 +0200

    initial project. add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main
+      className="flex justify-center items-center bg-gradient-to-br from-sky-400
+                via-rose-400 to-lime-400 h-[100vh] w-full"
+    >
+      <h1>Hello world</h1>
+    </main>
   );
 }
 
